fd5e724da468bb640a01902cf072dc26eee97c87
Woynshet Bilihatu <wbilihatu@gmail.com>
2021-12-08 14:16:05 +0300

    image picker is added

--- lib/screens/add_place.screen.dart
+++ lib/screens/add_place.screen.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: use_key_in_widget_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import '../widgets/image_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = "/add-place";
@@ -9,6 +10,7 @@ class AddPlaceScreen extends StatefulWidget {
 }
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +18,41 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
         title: Text("Add a New Place"),
       ),
       body: Column(
-        children: [],
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+              child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: _titleController,
+                    decoration: InputDecoration(label: Text("Title")),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ImageInput(),
+                  FlatButton(
+                    onPressed: () {},
+                    child: Text("addimage"),
+                  )
+                ],
+              ),
+            ),
+          )),
+          RaisedButton.icon(
+              color: Theme.of(context).primaryColor,
+              elevation: 0,
+              materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              onPressed: () {},
+              icon: Icon(Icons.add),
+              label: Text(
+                "Add Places",
+                style: TextStyle(color: Colors.white),
+              ))
+        ],
       ),
     );
   }
