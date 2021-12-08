70686b1c3a812e725aa2ab47e54973164de759e2
Woynshet Bilihatu <wbilihatu@gmail.com>
2021-12-08 17:11:59 +0300

    chaged file type for image

--- lib/Providers/greate_places.dart
+++ lib/Providers/greate_places.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_final_fields, unused_field
 
 import 'package:flutter/foundation.dart';
+import 'package:image_picker/image_picker.dart';
 import '../models/place.dart';
 
 // change notifier is a mixin that  allows you to call notifyListeners that is used by provider package to update places
@@ -11,5 +12,16 @@ class GreatePlaces with ChangeNotifier {
   //  a getter to displace
   List<Place> get items {
     return [..._items];
+  }
+
+  addPlace(String pickedTitle, XFile? pickedImage) {
+    final newPlace = Place(
+      id: DateTime.now().toString(),
+      image: pickedImage,
+      title: pickedTitle,
+      location: PlaceLocation(address: "null", latitude: 1221, longtude: 1221),
+    );
+    _items.add(newPlace);
+    notifyListeners();
   }
 }
