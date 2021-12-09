15f9c0c2d6cff20d279a9ba07c652f42bfea1db2
Woynshet Bilihatu <wbilihatu@gmail.com>
2021-12-09 10:18:22 +0300

    added callback 2 functions

--- lib/Providers/greate_places.dart
+++ lib/Providers/greate_places.dart
@@ -3,25 +3,64 @@
 import 'package:flutter/foundation.dart';
 import 'package:image_picker/image_picker.dart';
 import '../models/place.dart';
+import '../Helpers/db_helper.dart';
 
 // change notifier is a mixin that  allows you to call notifyListeners that is used by provider package to update places
 class GreatePlaces with ChangeNotifier {
-  // to store
   List<Place> _items = [];
 
-  //  a getter to displace
   List<Place> get items {
     return [..._items];
   }
 
-  addPlace(String pickedTitle, XFile? pickedImage) {
+  void addPlace(
+    String pickedTitle,
+    XFile? pickedImage,
+  ) {
     final newPlace = Place(
       id: DateTime.now().toString(),
       image: pickedImage,
       title: pickedTitle,
-      location: PlaceLocation(address: "null", latitude: 1221, longtude: 1221),
+      location:
+          PlaceLocation(address: "address", latitude: 1221, longtude: 1221),
     );
     _items.add(newPlace);
+    notifyListeners();
+    DBHelper.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image!.path,
+    });
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData('user_places');
+    _items = dataList
+        .map(
+          (item) => Place(
+            id: item['id'],
+            title: item['title'],
+            image: XFile(item['image']),
+            location:
+                PlaceLocation(address: "", latitude: 1221, longtude: 1221),
+          ),
+        )
+        .toList();
+    notifyListeners();
+  }
+
+  Future<void> placesss() async {
+    final maps = await DBHelper.getData('user_places');
+
+    _items = List.generate(maps.length, (i) {
+      return Place(
+        id: maps[i]['id'],
+        title: maps[i]['title'],
+        image: XFile(maps[i]['image']),
+        location: PlaceLocation(address: "", latitude: 1221, longtude: 1221),
+      );
+    });
+
     notifyListeners();
   }
 }
