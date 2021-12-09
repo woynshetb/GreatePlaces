e07eaae3e14d920fce0353ab013cf1f8aafa050b
Woynshet Bilihatu <wbilihatu@gmail.com>
2021-12-09 15:58:17 +0300

    database is updated

--- lib/Helpers/db_helper.dart
+++ lib/Helpers/db_helper.dart
@@ -9,7 +9,7 @@ class DBHelper {
     return sql.openDatabase(path.join(dbPath, 'places.db'),
         onCreate: (db, version) {
       return db.execute(
-          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT)');
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, location_latitude REAL, location_longtude REAL ,address TEXT)');
     }, version: 1);
   }
 
