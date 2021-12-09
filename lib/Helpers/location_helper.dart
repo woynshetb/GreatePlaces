10587710f3e1c74fd21c13eff3904db97aabd9a9
Woynshet Bilihatu <wbilihatu@gmail.com>
2021-12-09 16:02:21 +0300

    updated

---
+++ lib/Helpers/location_helper.dart
@@ -0,0 +1,10 @@
+const GOOGLE_API_KEY = 'AIzaSyBg9yn5JtQgKRFbg6FCTy4ewbF24kRuAYI';
+
+class LocationHelper {
+  static String generateLocationPreviewImage({
+    latitude,
+    longitude,
+  }) {
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+  }
+}
